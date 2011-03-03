cecb0f7a2eb6b39e5108a712882535b884029560
Nicolas <nico@rottenbytes.info>
2011-03-03 20:18:30 +0100

    rework & finished smith agent

--- plugins/agents/smith.rb
+++ plugins/agents/smith.rb
@@ -10,40 +10,46 @@ module MCollective
                 validate :name, String
                 validate :source, String
                 validate :method, String
-                # todo : use some authorization mechs here
-                # see http://www.devco.net/archives/2010/04/11
+                validate :reload, /(true|false)/
                 
                 update_methods = [ "http", "file"]
-                
+                                
                 if !update_methods.include? request[:method] then
                     reply.fail ":method must be one of " + update_methods.join(", ")
                 else
-                    destination = @config.libdir+"/mcollective/agent/"+request[:name]+".rb"
+                    destination = @config.libdir.to_s+"/mcollective/agent/"+request[:name]+".rb"
                     reply[:destination] = destination
-                    
-                    case request[:method]
-                        when "http":
-                            require 'open-uri'
-                            # Get the file from http (request[:source]) to its destination
-                            fp=open(destination,"w")
-                            fp.write(open(request[:source]).read)
-                            fp.close
-
-                            # is there a way to control this ??
-                            reply[:result] = true
-                            
-                        when "file":
-                            require 'ftools'
-                            # copy the file from request[:source] to the right place
-                            begin File.syscopy(request[:source],destination)
-                                reply[:result] = true
-                            rescue
-                                reply.fail "could not copy file while updating !"
-                            end
-                        end # end of when
                 end
+                    
+                case request[:method]
+                  when "http":
+                      require "rubygems"
+                      require 'open-uri'
+                      # Get the file from http (request[:source]) to its destination
+                      fp=open(destination,"w")
+
+                      fp.write(open(request[:source]).read)
+                      fp.close
+
+                      # is there a way to control this ??
+                      reply[:result] = true
+                      
+                  when "file":
+                      require 'ftools'
+                      # copy the file from request[:source] to the right place
+                      begin File.syscopy(request[:source],destination)
+                          reply[:result] = true
+                      rescue
+                          reply.fail "could not copy file while updating !"
+                      end
+                end # end of case
+   
+            if request[:reload] == "true" then
+              Log.instance.debug("Reload requested after update of an agent (#{request[:name]})")
+            end
+                
             end # end of updateagent_action method
             
-        end
-    end # end of module
+        end # end of class/class/module
+    end 
 end
--- plugins/agents/svnagent.rb
+++ plugins/agents/svnagent.rb
@@ -1,35 +1,35 @@
 module MCollective
-    module Agent
-        class Svnagent < RPC::Agent
-            metadata :name        => "svnagent",
-                     :description => "Agent to manage svn repos", 
-                     :author      => "Nicolas Szalay",
-                     :license     => "BSD",
-                     :version     => "0.1",
-                     :url         => "http://www.rottenbytes.info/",
-                     :timeout     => 60
+  module Agent
+    class Svnagent < RPC::Agent
+      metadata :name        => "svnagent",
+        :description => "Agent to manage svn repos", 
+        :author      => "Nicolas Szalay",
+        :license     => "BSD",
+        :version     => "0.1",
+        :url         => "http://www.rottenbytes.info/",
+        :timeout     => 60
         
-            def startup_hook
-                @timeout = 60
-            end
+        def startup_hook
+          @timeout = 60
+        end
         
-            action "update" do
-                validate :path, String
+        action "update" do
+          validate :path, String
 
-   		revision=`cd #{request[:path]} && svn up` 
-		# send back revision number
-		reply[:data]=revision.chomp.split(" ")[-1].gsub(".","")
-	    end
+          revision=`cd #{request[:path]} && svn up` 
+          # send back revision number
+          reply[:data]=revision.chomp.split(" ")[-1].gsub(".","")
+        end
 
-	    action "getrevision" do
-		validate :path, String
+        action "getrevision" do
+		      validate :path, String
 
-		# accomodate english & french locales 
-		revision=`cd #{request[:path]} && svn info | grep R.vision | head -1`
-		reply[:revision]=revision.chomp.split(" ")[-1]
-	    end
+		      # accomodate english & french locales 
+		      revision=`cd #{request[:path]} && svn info | grep R.vision | head -1`
+		      reply[:revision]=revision.chomp.split(" ")[-1]
+        end
 	        
         # end of class Svnagent
-        end
     end
+  end
 end
