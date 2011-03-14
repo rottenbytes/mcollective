8651c058e1fefc92aa2e699bf7ef93903f9b34dd
Nicolas <nico@rottenbytes.info>
2011-03-14 22:31:26 +0100

    added reloading + action delete

--- plugins/agents/smith.rb
+++ plugins/agents/smith.rb
@@ -1,12 +1,12 @@
 module MCollective
     module Agent
         class Smith < RPC::Agent
-            def report_action
+            action "report" do
                 reply[:libdir] = @config.libdir
                 reply[:agents] = Agents.agentlist
             end
                       
-            def updateagent_action
+            action "updateagent" do
                 validate :name, String
                 validate :source, String
                 validate :method, String
@@ -46,9 +46,26 @@ module MCollective
    
             if request[:reload] == "true" then
               Log.instance.debug("Reload requested after update of an agent (#{request[:name]})")
+              ::Process.kill("USR1", $$)
             end
-                
-            end # end of updateagent_action method
+          end # end of updateagent action/method
+            
+          action "deleteagent" do
+            validate :name, String
+            validate :reload, /(true|false)/
+            
+            agentfile = @config.libdir.to_s+"/mcollective/agent/"+request[:name]+".rb"
+            
+            if File.exists?(agentfile) then
+              # delete the file
+              File.delete(agentfile)
+            end
+            
+            if request[:reload] == "true" then
+              Log.instance.debug("Reload requested after deletion of an agent (#{request[:name]})")
+              ::Process.kill("USR1", $$)
+            end
+          end # end of deleteagent action/methodd
             
         end # end of class/class/module
     end 
